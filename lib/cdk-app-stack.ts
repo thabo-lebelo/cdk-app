81ca007a9873f6bc50cf4be4e330a3ed4eba87ed
Thabo Lebelo <thabolebelo@icloud.com>
2022-02-13 18:30:21 +0200

    Add S3 bucket resource

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class CdkAppStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		// The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		// example resource
+		// const queue = new sqs.Queue(this, 'CdkAppQueue', {
+		//   visibilityTimeout: cdk.Duration.seconds(300)
+		// });
+
+		const myBucket = new s3.Bucket(this, "thabo-cdk-bucket");
+	}
 }
